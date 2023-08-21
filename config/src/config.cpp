2a68eccefe3cdb9df10f863fbdb31f5f79e1cf98
Samiullah Mohammed <samiull@amazon.com>
2023-08-21 17:14:49 +0000

    Read configuration from config file
    This affects EKS specifically, without a config file, the mode defaults
    to the previous ECS mode.
    
    Testing:
    I tested with the following conf file and EKS config
    /etc/credentials_fetcher.conf
    
     credentials_fetcher_mode = "domain_joined_and_eks_mode"
     credentials_fetcher_kubeconfig_path = "/etc/credentials_fetcher_kubeconfig.json"
    
    $ sudo cat /etc/credentials_fetcher_kubeconfig.json
    {
      "ServiceAccountMappings": [
        {
          "ServiceAccountName": "webapp01",
          "path_to_cred_spec_json": "/home/ec2-user/customertest_webapp01.json",
          "domainless_user": "",
          "kube_context": [
            {
              "kube_context_name": "name",
              "path_to_kube_pod_yaml_file": "/home/ec2-user/pod_with_secret.yaml",
              "path_to_kube_secret_yaml_file": "/home/ec2-user/secret1.yaml"
            }
          ]
        }
      ]
    }
    
    $ sudo cat /home/ec2-user/customertest_webapp01.json
    {
      "CmsPlugins":  [
        "ActiveDirectory"
      ],
      "DomainJoinConfig":  {
        "Sid":  "S-1-5-21-4217655605-3681839426-3493040985",
        "MachineAccountName":  "webapp01",
        "Guid":  "af602f85-d754-4eea-9fa8-fd76810485f1",
        "DnsTreeName":  "customertest.local",
        "DnsName":  "customertest.local",
        "NetBiosName":  "customertest"
      },
      "ActiveDirectoryConfig":  {
        "GroupManagedServiceAccounts":  [
          {
            "Name":  "webapp01",
            "Scope":  "customertest.local"
          },
          {
            "Name":  "webapp01",
            "Scope":  "customertest"
          }
        ]
      }
    }

--- config/src/config.cpp
+++ config/src/config.cpp
@@ -1,52 +1,13 @@
 #include "daemon.h"
 
+#include <boost/program_options.hpp>
+
 static std::string global_config_file_name( "/etc/credentials_fetcher.conf" );
 static std::string credentials_fetcher_mode_key( "credentials_fetcher_mode" );
 static std::string domain_joined_and_grpc_mode( "domain_joined_and_grpc_mode" );
 static std::string domain_joined_and_eks_mode( "domain_joined_and_eks_mode" );
 static std::string kubeconfig_path_key( "credentials_fetcher_kubeconfig_path" );
 static std::string krbdir( "/var/credentials-fetcher/krbdir" );
-
-/* Reads global config from /etc/credentials_fetcher.conf
- * If credentials_fetcher_mode is not set in /etc/credentials_fetcher.conf, daemon will
- * be in "domain_joined_and_grpc_mode", i.e ECS mode
- */
-void read_global_config(creds_fetcher::Daemon& cf_daemon)
-{
-    std::ifstream global_config_file( global_config_file_name );
-    std::string line;
-    std::vector<std::string> results;
-
-    while ( std::getline( global_config_file, line ) )
-    {
-        // TBD:: Use json parser later
-        boost::split( results, line, []( char c ) { return c == '='; } );
-        std::string key = results[0];
-        std::string value = results[1];
-        if ( credentials_fetcher_mode_key.compare( key ) == 0 )
-        {
-            value.erase( std::remove( value.begin(), value.end(), '"' ), value.end() );
-            if ( key == credentials_fetcher_mode_key )
-            {
-		std:: string msg = "Option credentials_fetcher_mode is set to " + value;
-                if ( value == domain_joined_and_eks_mode )
-                {
-                    cf_daemon.use_kube = true;
-                }
-                std::cout << msg << std::endl;
-                cf_daemon.cf_logger.logger( LOG_ERR, msg.c_str(), 0 );
-            }
-            if ( key == kubeconfig_path_key )
-            {
-                cf_daemon.kube_config_file_path = value;
-		std::string msg = "Option credentials_fetcher_kubeconfig_path is set to " + value;
-                std::cout << msg << std::endl;
-                cf_daemon.cf_logger.logger( LOG_ERR, msg.c_str(), 0 );
-            }
-        }
-    }
-    cf_daemon.krb_files_dir = krbdir;
-}
 
 /**
  * This function has options used to invoke the daemon such as
@@ -62,8 +23,6 @@ int parse_options( int argc, const char* argv[], creds_fetcher::Daemon& cf_daemo
     const char* ecs_config_file_name = "/etc/ecs/ecs.config"; // TBD:: Add commandline if needed
     std::string domainless_gmsa_field( "CREDENTIALS_FETCHER_SECRET_NAME_FOR_DOMAINLESS_GMSA" );
 
-    read_global_config(cf_daemon);
-
     try
     {
         namespace po = boost::program_options;
@@ -78,6 +37,12 @@ int parse_options( int argc, const char* argv[], creds_fetcher::Daemon& cf_daemo
                 "Name of secret containing username/password in AWS Secrets Manager (in same "
                 "region)" );
 
+        po::options_description fileOptions{ "File" };
+        fileOptions.add_options()( "credentials_fetcher_mode", po::value<std::string>(),
+                                   "credentials_fetcher_mode" );
+        fileOptions.add_options()( "credentials_fetcher_kubeconfig_path", po::value<std::string>(),
+                                   "credentials_fetcher_kubeconfig_path" );
+
         /**
          * Calls to store, parse_command_line and notify functions
          * cause the vm variable to contain all the options found on the command line
@@ -85,6 +50,45 @@ int parse_options( int argc, const char* argv[], creds_fetcher::Daemon& cf_daemo
         po::variables_map vm;
         po::store( po::parse_command_line( argc, argv, desc ), vm );
         po::notify( vm );
+
+        std::ifstream ifs{ global_config_file_name };
+        if ( ifs )
+        {
+            store( po::parse_config_file( ifs, fileOptions ), vm );
+            po::notify( vm );
+            std::string msg = "Using global config file " + global_config_file_name;
+            std::cout << msg << std::endl;
+        }
+
+        if ( vm.count( "credentials_fetcher_mode" ) )
+        {
+            std::string msg =
+                "credentials_fetcher_mode = " + vm["credentials_fetcher_mode"].as<std::string>();
+            std::cout << msg << std::endl;
+            std::string mode = "\"" + domain_joined_and_eks_mode + "\"";
+            if ( vm["credentials_fetcher_mode"].as<std::string>() == mode )
+            {
+                msg = "From global config file, credentials_fetcher_mode = " +
+                      vm["credentials_fetcher_mode"].as<std::string>();
+                std::cout << msg << std::endl;
+                cf_daemon.use_kube = true;
+                if ( vm.count( "credentials_fetcher_kubeconfig_path" ) )
+                {
+                    cf_daemon.kube_config_file_path =
+                        vm["credentials_fetcher_kubeconfig_path"].as<std::string>();
+                    cf_daemon.kube_config_file_path.erase( cf_daemon.kube_config_file_path.begin(),
+                                                           cf_daemon.kube_config_file_path.begin() +
+                                                               1 );
+                    cf_daemon.kube_config_file_path.erase( cf_daemon.kube_config_file_path.end() -
+                                                               1,
+                                                           cf_daemon.kube_config_file_path.end() );
+                    std::string msg =
+                        "From global config file, credentials_fetcher_kubeconfig_path = " +
+                        cf_daemon.kube_config_file_path;
+                    std::cout << msg << std::endl;
+                }
+            }
+        }
 
         if ( vm.count( "help" ) )
         {
