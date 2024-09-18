4fd707559a83fd0562e85f073d8eb3458d43c502
Samiullah Mohammed <samiull@amazon.com>
2024-09-18 11:39:04 -0700

    Fix start_stack.sh

--- cdk/start_stack.sh
+++ cdk/start_stack.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Please edit the file to add your AWS account number below"
-cdk bootstrap --trust=<Add your AWS account> --cloudformation-execution-policies=arn:aws:iam::aws:policy/AdministratorAccess --verbose && cdk deploy
+cdk bootstrap aws://<Add your AWS account number here>/us-west-1 --trust=<Add your AWS account number here> --cloudformation-execution-policies=arn:aws:iam::aws:policy/AdministratorAccess --verbose && cdk synth && cdk deploy
